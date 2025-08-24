b530e5b48c470b561150e1b4cbe49ce81431e273
JAYARANI555 <ranijaya0292@gmail.com>
2025-08-24 05:39:13 +0000

    second commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mcq-generator",
+    version="0.0.1",
+    author='jaya rani',
+    author_email='ranijaya0292@gmil.com',
+    packages=find_packages(),
+    install_requires=[
+        "openai",
+        "langchain",
+        "streamlit",
+        "python-dotenv"
+    ]
+)
